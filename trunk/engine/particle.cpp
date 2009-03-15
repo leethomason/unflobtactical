95f9862366c7c6ec2d0bb68e78b87795d77b4a07
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2009-03-15 03:04:32 +0000

    missing files

---
+++ trunk/engine/particle.cpp
@@ -0,0 +1,424 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "particle.h"
+#include "../grinliz/glgeometry.h"
+#include "platformgl.h"
+#include "surface.h"
+#include "camera.h"
+
+using namespace grinliz;
+
+ParticleSystem::ParticleSystem()
+{
+	for( int i=0; i<NUM_PRIMITIVES; ++i ) {
+		nParticles[i] = 0;
+		particleTypeArr[i].Init( 0, 1.0f );
+	}
+}
+
+
+ParticleSystem::~ParticleSystem()
+{
+}
+
+
+void ParticleSystem::InitPoint( const Texture* texture )
+{
+	particleTypeArr[POINT].Init( texture, 20.0f );
+}
+
+
+void ParticleSystem::InitQuad( const Texture* texture )
+{
+	particleTypeArr[QUAD].Init( texture, 1.0f );
+}
+
+
+void ParticleSystem::Update( U32 msec )
+{
+	float sec = (float)msec / 1000.0f;
+
+	for( int i=0; i<NUM_PRIMITIVES; ++i ) {
+		Particle* pStart	= 0;
+		if ( i==POINT )
+			pStart = pointBuffer;
+		else if ( i==QUAD )
+			pStart = quadBuffer;
+		GLASSERT( pStart );
+		Particle* p = pStart;
+			
+		Particle* pEnd	= pStart + nParticles[i];
+
+		while( p < pEnd ) {
+			p->age += msec;
+			if ( p->age > p->lifetime ) {
+				// swap with end.
+				grinliz::Swap( p, pEnd-1);
+				--pEnd;
+				continue;
+			}
+			// apply effect of time
+			p->pos += p->vel*sec;
+			p->color += p->colorVel*sec;
+			++p;
+		}
+		nParticles[i] = pEnd - pStart;
+	}
+}
+
+
+void ParticleSystem::Emit(	int primitive,					// POINT or QUAD
+							int type,						// FIRE, SMOKE
+							int count,						// number of particles to create
+							int config,						// PARTICLE_RAY, etc.
+							const Color4F& color,
+							const Color4F& colorVelocity,
+							const grinliz::Vector3F& pos,	// origin
+							float posFuzz,
+							const grinliz::Vector3F& vel,	// velocity
+							float velFuzz,
+							U32 lifetime )
+{
+	GLASSERT( primitive >= 0 && primitive < NUM_PRIMITIVES );
+	GLASSERT( type >=0 && type < NUM_TYPE );
+	GLASSERT( config >= 0 && config <= PARTICLE_SPHERE );
+
+	const int MAX_PARTICLES[2] = { MAX_POINT_PARTICLES, MAX_QUAD_PARTICLES };
+	Particle* particles[3] = { pointBuffer, quadBuffer };
+
+	GLASSERT( count < MAX_PARTICLES[primitive] );
+	lifetime = grinliz::Clamp( lifetime, (U32)16, (U32)0xffff );
+	
+	int insert = nParticles[primitive];
+	if ( nParticles[primitive] + count > MAX_PARTICLES[primitive] ) {
+		insert = MAX_PARTICLES[primitive] - count;
+		nParticles[primitive] = MAX_PARTICLES[primitive];
+	}
+	else {
+		nParticles[primitive] += count;
+	}
+
+
+	Vector3F posP, velP, normal;
+	Color4F colP;
+	U16 lifeP;
+	const float len = vel.Length();
+	normal = vel;
+	normal.Normalize();
+
+	for( int i=0; i<count; ++i ) {
+		switch (config) {
+			case PARTICLE_RAY:
+				velP = vel;
+				velP.x += velFuzz*(-0.5f + rand.Uniform());
+				velP.y += velFuzz*(-0.5f + rand.Uniform());
+				velP.z += velFuzz*(-0.5f + rand.Uniform());
+				break;
+
+			case PARTICLE_HEMISPHERE:
+				rand.NormalVector( &velP.x, 3 );
+				if ( DotProduct( velP, normal ) < 0.0f ) {
+					velP = -velP;
+				}
+				velP.x = velP.x*len + velFuzz*(-0.5f + rand.Uniform());
+				velP.y = velP.y*len + velFuzz*(-0.5f + rand.Uniform());
+				velP.z = velP.z*len + velFuzz*(-0.5f + rand.Uniform());
+				break;
+
+			case PARTICLE_SPHERE:
+				rand.NormalVector( &velP.x, 3 );
+				velP.x = velP.x*len + velFuzz*(-0.5f + rand.Uniform());
+				velP.y = velP.y*len + velFuzz*(-0.5f + rand.Uniform());
+				velP.z = velP.z*len + velFuzz*(-0.5f + rand.Uniform());
+				break;
+
+			default:
+				GLASSERT( 0 );
+				break;
+		};
+
+		posP.x = pos.x + posFuzz*(-0.5f + rand.Uniform() );
+		posP.y = pos.y + posFuzz*(-0.5f + rand.Uniform() );
+		posP.z = pos.z + posFuzz*(-0.5f + rand.Uniform() );
+
+		const int LFUZZ = 64;
+		lifeP = (lifetime * ((256-LFUZZ) + (rand.Rand()&(LFUZZ-1)))) >> 8;
+
+		const float CFUZZ_MUL = 0.90f;
+		const float CFUZZ_ADD = 0.20f;
+
+		for( int j=0; j<3; ++j ) {
+			colP.X(j) = color.X(j)*CFUZZ_MUL + rand.Uniform()*CFUZZ_ADD;
+		}
+		colP.w = color.w;	// don't fuzz alpha
+
+		GLASSERT( insert < MAX_PARTICLES[primitive] );
+		Particle* p = particles[primitive] + insert;
+		++insert;
+
+		p->pos = posP;
+		p->color = colP;
+		p->vel = velP;
+		p->colorVel = colorVelocity;
+		p->age = 0;
+		p->lifetime = lifeP;
+		p->type = (U8)type;
+		p->subType = (U8)(rand.Rand()>>30);
+	}
+}
+
+
+void ParticleSystem::EmitFlame( U32 delta, const Vector3F& _pos )
+{
+	// flame, smoke, particle
+	U32 count[3];
+	const U32 interval[3] = { 350, 600, 200 };
+
+	// If the delta is 200, there is a 200/250 chance of it being in this delta
+	for( int i=0; i<3; ++i ) {
+		count[i] = delta / interval[i];
+		U32 remain = delta - count[i]*interval[i];
+
+		U32 v = rand.Rand() % interval[i];
+		if ( v < remain ) {
+			++count[i];
+		}
+	}
+
+	
+	// Flame
+	{
+		Vector3F pos = _pos;
+		pos.y += 0.3f;
+
+		const Color4F color		= { 1.0f, 1.0f, 1.0f, 1.0f };
+		const Color4F colorVec	= { 0.0f, 0.0f, 0.0f, -0.4f };
+		Vector3F velocity		= { 0.0f, 1.0f, 0.0f };
+
+		if ( (rand.Rand()>>28)==0 ) {
+			velocity.y = 0.1f;
+		}
+
+		for( U32 i=0; i<count[0]; ++i ) {
+			Emit(	ParticleSystem::QUAD,
+					ParticleSystem::FIRE,
+					1,
+					ParticleSystem::PARTICLE_RAY,
+					color,		colorVec,
+					pos,		0.4f,	
+					velocity,	0.3f,
+					2000 );		
+		}
+	}
+	
+	// Smoke
+	{
+		Vector3F pos = _pos;
+		pos.y += 1.0f;
+
+		const Color4F color		= { 0.5f, 0.5f, 0.5f, 1.0f };
+		const Color4F colorVec	= { -0.1f, -0.1f, -0.1f, -0.07f };
+		Vector3F velocity		= { 0.0f, 0.8f, 0.0f };
+
+		for( U32 i=0; i<count[1]; ++i ) {
+			Emit(	ParticleSystem::QUAD,
+					ParticleSystem::SMOKE,
+					1,
+					ParticleSystem::PARTICLE_RAY,
+					color,		colorVec,
+					pos,		0.6f,	
+					velocity,	0.2f,
+					5000 );		
+		}
+	}
+	
+	// Sparkle
+	{
+		Vector3F pos = _pos;
+
+		const Color4F color		= { 1.0f, 0.3f, 0.1f, 1.0f };
+		const Color4F colorVec	= { 0.0f, -0.1f, -0.1f, -0.1f };
+		Vector3F velocity		= { 0.0f, 1.5f, 0.0f };
+
+		for( U32 i=0; i<count[2]; ++i ) {
+			Emit(	ParticleSystem::POINT,
+					0,
+					1,
+					ParticleSystem::PARTICLE_RAY,
+					color,		colorVec,
+					pos,		0.05f,	
+					velocity,	0.3f,
+					2000 );		
+		}
+	}
+}
+
+
+void ParticleSystem::Draw( const Vector3F* eyeDir )
+{
+	glEnable( GL_BLEND );
+	glDepthMask( GL_FALSE );
+
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+	DrawPointParticles();
+
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE );
+	DrawQuadParticles( eyeDir );
+
+	glDepthMask( GL_TRUE );
+	glDisable( GL_BLEND );
+}
+
+
+void ParticleSystem::DrawPointParticles()
+{
+	CHECK_GL_ERROR;
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glDisableClientState( GL_NORMAL_ARRAY );
+	glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+	glEnableClientState( GL_COLOR_ARRAY );
+	
+	glEnable( GL_TEXTURE_2D );
+
+	glEnable(GL_POINT_SPRITE);
+	glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE);
+
+	CHECK_GL_ERROR;
+	if ( nParticles[POINT] > 0 ) {
+		glBindTexture( GL_TEXTURE_2D, particleTypeArr[POINT].texture->glID );
+		glPointSize( particleTypeArr[POINT].size );
+		float maxSize = 40.0f;
+		glPointParameterfv( GL_POINT_SIZE_MAX, &maxSize );
+		CHECK_GL_ERROR;
+
+	    float quadratic[] =  { 1.0f, 0.0f, 0.01f };
+		glPointParameterfv( GL_POINT_DISTANCE_ATTENUATION_ARB, quadratic );
+		CHECK_GL_ERROR;
+		
+		U8* vPtr = (U8*)pointBuffer + 0;
+		U8* cPtr = (U8*)pointBuffer + 12;
+
+		glVertexPointer( 3, GL_FLOAT, sizeof(Particle), vPtr );
+		glColorPointer( 4, GL_FLOAT, sizeof(Particle), cPtr );
+		CHECK_GL_ERROR;
+		glDrawArrays( GL_POINTS, 0, nParticles[POINT] );
+		CHECK_GL_ERROR;
+	}
+
+	glDisable( GL_POINT_SPRITE );
+
+	// Restore standard state.
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glEnableClientState( GL_NORMAL_ARRAY );
+	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+	glDisableClientState( GL_COLOR_ARRAY );
+	CHECK_GL_ERROR;
+}
+
+
+void ParticleSystem::DrawQuadParticles( const Vector3F* eyeDir )
+{
+	if ( nParticles[QUAD] == 0 ) {
+		return;
+	}
+
+	QuadVertex base[4];
+
+	base[0].pos = -eyeDir[Camera::RIGHT]*0.5f - eyeDir[Camera::UP]*0.5f;
+	base[1].pos =  eyeDir[Camera::RIGHT]*0.5f - eyeDir[Camera::UP]*0.5f;
+	base[2].pos =  eyeDir[Camera::RIGHT]*0.5f + eyeDir[Camera::UP]*0.5f;
+	base[3].pos = -eyeDir[Camera::RIGHT]*0.5f + eyeDir[Camera::UP]*0.5f;
+	
+	int index = 0;
+	int vertex = 0;
+	GLASSERT( nParticles[QUAD] <= MAX_QUAD_PARTICLES );
+
+
+	for( int i=0; i<nParticles[QUAD]; ++i ) 
+	{
+		const int type	  = quadBuffer[i].type;
+		const int subType = quadBuffer[i].subType;
+
+		const float tx = 0.25f * (float)subType;
+		const float ty = 0.25f * (float)type;
+		
+		const float size = particleTypeArr[QUAD].size;
+		const Vector3F& pos  = quadBuffer[i].pos;
+		const Color4F& color = quadBuffer[i].color;
+
+		// Set up the particle that everything else is derived from:
+		quadIndexBuffer[index++] = vertex+0;
+		quadIndexBuffer[index++] = vertex+1;
+		quadIndexBuffer[index++] = vertex+2;
+
+		quadIndexBuffer[index++] = vertex+0;
+		quadIndexBuffer[index++] = vertex+2;
+		quadIndexBuffer[index++] = vertex+3;
+
+		QuadVertex* pV = &vertexBuffer[vertex];
+
+		pV->pos = base[0].pos*size + pos;
+		pV->tex.Set( tx, ty );
+		pV->color = color;
+		++pV;
+
+		pV->pos = base[1].pos*size + pos;
+		pV->tex.Set( tx+0.25f, ty );
+		pV->color = color;
+		++pV;
+
+		pV->pos = base[2].pos*size + pos;
+		pV->tex.Set( tx+0.25f, ty+0.25f );
+		pV->color = color;
+		++pV;
+
+		pV->pos = base[3].pos*size + pos;
+		pV->tex.Set( tx, ty+0.25f );
+		pV->color = color;
+		++pV;
+
+		vertex += 4;
+	}
+
+	CHECK_GL_ERROR;
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glDisableClientState( GL_NORMAL_ARRAY );
+	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+	glEnableClientState( GL_COLOR_ARRAY );
+
+	glEnable( GL_TEXTURE_2D );
+	glBindTexture( GL_TEXTURE_2D, particleTypeArr[QUAD].texture->glID );
+
+	CHECK_GL_ERROR;
+	U8* vPtr = (U8*)vertexBuffer + 0;
+	U8* tPtr = (U8*)vertexBuffer + 12;
+	U8* cPtr = (U8*)vertexBuffer + 20;
+
+	CHECK_GL_ERROR;
+	glVertexPointer(   3, GL_FLOAT, sizeof(QuadVertex), vPtr );
+	glTexCoordPointer( 2, GL_FLOAT, sizeof(QuadVertex), tPtr );
+	glColorPointer(    4, GL_FLOAT, sizeof(QuadVertex), cPtr );
+
+	glDrawElements( GL_TRIANGLES, nParticles[QUAD]*6, GL_UNSIGNED_SHORT, quadIndexBuffer );
+	CHECK_GL_ERROR;
+
+	// Restore standard state.
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glEnableClientState( GL_NORMAL_ARRAY );
+	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+	glDisableClientState( GL_COLOR_ARRAY );
+	CHECK_GL_ERROR;
+}
---
+++ trunk/engine/particle.h
@@ -0,0 +1,125 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef UFOTACTICAL_PARTICLE_INCLUDED
+#define UFOTACTICAL_PARTICLE_INCLUDED
+
+#include "../grinliz/glvector.h"
+#include "../grinliz/glrandom.h"
+#include "vertex.h"
+
+class Texture;
+
+class ParticleSystem
+{
+public:
+
+
+	enum {
+		MAX_POINT_PARTICLES = 400,
+		MAX_QUAD_PARTICLES = 100
+	};
+
+	ParticleSystem();
+	~ParticleSystem();
+
+	void InitPoint( const Texture* texture );
+	void InitQuad( const Texture* texture );
+
+	// texture particles
+	enum {
+		FIRE,			
+		SMOKE,
+		NUM_TYPE,
+	};
+
+	enum {
+		POINT,
+		QUAD,
+		NUM_PRIMITIVES
+	};
+
+	enum {
+		PARTICLE_RAY,
+		PARTICLE_HEMISPHERE,
+		PARTICLE_SPHERE
+	};
+
+	void Emit(	int primitive,					// POINT or QUAD
+				int type,						// FIRE, SMOKE
+				int count,						// number of particles to create
+				int configuration,				// PARTICLE_RAY, etc.
+				const Color4F& color,
+				const Color4F& colorVelocity,
+				const grinliz::Vector3F& pos,	// origin
+				float posFuzz,
+				const grinliz::Vector3F& vel,	// velocity
+				float velFuzz,
+				U32 lifetime );					// lifetime in milliseconds
+
+	void EmitFlame( U32 delta, const grinliz::Vector3F& pos );
+
+	void Update( U32 timePassed );
+	void Draw( const grinliz::Vector3F* eyeDir );
+
+private:
+
+	struct ParticleType 
+	{
+		const Texture* texture;
+		float size;
+		
+		void Init( const Texture* texture, float size ) {
+			this->texture = texture;
+			this->size = size;
+		}
+	};
+
+	struct Particle
+	{
+		// streamed to GL
+		grinliz::Vector3F	pos;		
+		Color4F				color;
+
+		grinliz::Vector3F vel;			// units / second
+		grinliz::Vector4F colorVel;		// units / second
+		U16 age;						// milliseconds
+		U16 lifetime;					// milliseconds
+		U8	type;	
+		U8	subType;
+	};
+
+	struct QuadVertex
+	{
+		grinliz::Vector3F	pos;
+		grinliz::Vector2F	tex;
+		Color4F				color;
+	};
+
+	void DrawPointParticles();
+	void DrawQuadParticles( const grinliz::Vector3F* eyeDir );
+
+	grinliz::Random rand;
+	int nParticles[NUM_PRIMITIVES];
+
+	ParticleType	particleTypeArr[ NUM_PRIMITIVES ];
+	Particle		pointBuffer[ MAX_POINT_PARTICLES ];
+	Particle		quadBuffer[ MAX_QUAD_PARTICLES ];
+
+	QuadVertex		vertexBuffer[ MAX_QUAD_PARTICLES*4];
+	U16				quadIndexBuffer[ MAX_QUAD_PARTICLES*6 ];
+};
+
+#endif // UFOTACTICAL_PARTICLE_INCLUDED
