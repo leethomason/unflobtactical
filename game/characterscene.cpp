706255a9597b5ac3ce08893e77a6127b4889a02f
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2009-03-24 03:58:08 +0000

    missing files

---
+++ game/characterscene.cpp
@@ -0,0 +1,45 @@
+#include "characterscene.h"
+#include "game.h"
+#include "cgame.h"
+#include "../engine/uirendering.h"
+
+
+CharacterScene::CharacterScene( Game* _game ) : Scene( _game )
+{
+	engine = &_game->engine;
+
+	Texture* t = game->GetTexture( "icons" );	
+	widgets = new UIButtonBox( t );
+
+	int icons[] = { UIButtonBox::ICON_PLAIN };
+	const char* iconText[] = { "back" };
+	widgets->SetButtons( icons, iconText, 1 );
+}
+
+
+CharacterScene::~CharacterScene()
+{
+	delete widgets;
+}
+
+
+void CharacterScene::Activate()
+{
+	engine->EnableMap( false );
+}
+
+
+void CharacterScene::DrawHUD()
+{
+	int h = engine->Width();
+	int w = engine->Height();
+	int rotation = game->Rotation();
+	widgets->Draw( w, h, rotation );
+}
+
+void CharacterScene::Tap(	int count, 
+							const grinliz::Vector2I& screen,
+							const grinliz::Ray& world )
+{
+	game->PopScene();
+}
---
+++ game/characterscene.h
@@ -0,0 +1,38 @@
+#ifndef UFO_ATTACK_CHARACTER_SCENE_INCLUDED
+#define UFO_ATTACK_CHARACTER_SCENE_INCLUDED
+
+#include "scene.h"
+#include "../engine/fixedgeom.h"
+
+class UIButtonBox;
+
+class CharacterScene : public Scene
+{
+public:
+	CharacterScene( Game* _game );
+	virtual ~CharacterScene();
+
+	virtual void Activate();
+	virtual void DeActivate()									{}
+
+	// UI
+	virtual void Tap(	int count, 
+						const grinliz::Vector2I& screen,
+						const grinliz::Ray& world );
+
+	virtual void Drag(	int action,
+						const grinliz::Vector2I& screenRaw )	{}
+	virtual void Zoom( int action, int distance )				{}
+	virtual void CancelInput()									{}
+
+	// Rendering
+	virtual void DoTick( U32 currentTime, U32 deltaTime )		{}
+	virtual void DrawHUD();
+
+protected:
+	Engine* engine;
+	UIButtonBox* widgets;
+};
+
+
+#endif // UFO_ATTACK_CHARACTER_SCENE_INCLUDED
