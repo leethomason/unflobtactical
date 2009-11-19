2d4fb2a71f0eaf5b9ca0c278b74f33375428e65f
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2009-11-19 03:24:24 +0000

    missing file

---
+++ trunk/grinliz/glfixed.cpp
@@ -0,0 +1,25 @@
+#include "glfixed.h"
+using namespace grinliz;
+
+/*static*/ bool Fixed::approxSqrtTableInit = false;
+/*static*/ Fixed Fixed::approxSqrtTable[ SQRT_TABLE_SIZE ];
+
+Fixed Fixed::ApproxSqrt()
+{
+	if ( !approxSqrtTableInit ) {
+		for( int i=0; i<SQRT_TABLE_SIZE; ++i ) {
+			Fixed v = Fixed(i) / Fixed( SQRT_TABLE_SIZE-1 );
+			approxSqrtTable[i] = v.Sqrt();
+		}
+		approxSqrtTableInit = true;
+	}
+
+	GLASSERT( x >= 0 && x <= 65536 );
+
+	//static const 1.0/(SQRT_TABLE_SIZE-1) )
+	static const S32 INV = 1040;
+	S32 i = INV * x;
+	i >>= 10;	// 2^16 -> 2^6
+	GLASSERT( i>=0 && i<SQRT_TABLE_SIZE );
+	return approxSqrtTable[i];
+}
