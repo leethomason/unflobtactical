0477587c72a8f17aee72f22b668730ab80a8f916
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2010-06-04 20:20:07 +0000

    tweaks to build.

--- buildrelease.bat
+++ buildrelease.bat
@@ -2,4 +2,6 @@ del /Q UFOAttack\*.*
 del /Q UFOAttack\res\*.*
 copy .\res\*.* UFOAttack\res\*.*
 copy c:\bin\SDL.dll UFOAttack
+copy C:\windows\SYSTEM32\msvcp100.dll UFOAttack
+copy C:\windows\SYSTEM32\msvcr100.dll UFOAttack
 copy .\win32\Release\UFOAttack.exe UFOAttack
