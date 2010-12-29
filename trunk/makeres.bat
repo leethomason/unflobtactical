0ad33a72d1bbee63f65c24848dd14abe297e8521
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2010-12-29 01:19:38 +0000

    missing file

---
+++ trunk/makeres.bat
@@ -0,0 +1,4 @@
+.\ufobuilder\win32\Debug\ufobuilder.exe .\resin\ default.xml
+copy .\res\uforesource.db .\android\ufoattack_1\res\raw\uforesource.png
+copy .\resin\androidicon48.png C:\src\unflbtactical\android\ufoattack_1\res\drawable-mdpi\
+
