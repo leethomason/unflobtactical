0ceb1212c0ca9d2f5e72c7ee37a6cafa57926b75
leethomason@gmail.com <leethomason@gmail.com@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2011-01-04 07:09:00 +0000

    Added the desert tiles. Modified game to allow desert to play.

---
+++ resin/tiletotile.bat
@@ -0,0 +1,4 @@
+copy %1_16_TILE_00.xml     %1_16_TILE_%2.xml
+copy %1_16_TILE_00_DAY.png %1_16_TILE_%2_DAY.png
+copy %1_16_TILE_00_NGT.png %1_16_TILE_%2_NGT.png
+copy %1_16_TILE_00_TEX.psd %1_16_TILE_%2_TEX.psd
