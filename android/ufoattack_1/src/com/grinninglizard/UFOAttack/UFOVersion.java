b7123947b4f6ceeaf340c80e1b8af3df5e8736ce
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2011-08-02 04:24:25 +0000

    After release fixes.

--- android/ufoattack_1/src/com/grinninglizard/UFOAttack/UFOVersion.java
+++ android/ufoattack_1/src/com/grinninglizard/UFOAttack/UFOVersion.java
@@ -1,4 +1,4 @@
 package com.grinninglizard.UFOAttack; 
 public class UFOVersion { 
-public static final int VERSION=660; 
+public static final int VERSION=672; 
 } 
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-const int VERSION=660; 
+const int VERSION=672; 
