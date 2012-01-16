a0644994de0daf9835484d8b6ca83919a39c9d33
leethomason <leethomason@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2012-01-16 16:32:56 +0000

    missing files

---
+++ branches/refactor27/engine/settings.h
@@ -0,0 +1,61 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef UFO_SETTINGS_INCLUDED
+#define UFO_SETTINGS_INCLUDED
+
+#include "../grinliz/gldebug.h"
+#include "../grinliz/gltypes.h"
+#include "../grinliz/glstringutil.h"
+#include <stdio.h>
+
+class TiXmlElement;
+
+
+class SettingsManager
+{
+public:
+	static SettingsManager* Instance()	{ GLASSERT( instance );	return instance; }
+
+	static void Create( const char* savepath );
+	static void Destroy();
+
+	bool GetAudioOn() const				{ return audioOn != 0; }
+	void SetAudioOn( bool value );
+
+	// read-write
+	void SetNumWalkingMaps(int nMaps );
+	int  GetNumWalkingMaps() const		{ return nWalkingMaps; }
+
+protected:
+	SettingsManager( const char* path );
+	virtual ~SettingsManager()	{ instance = 0; }
+	
+	void Load();
+	void Save();
+
+	virtual void ReadAttributes( const TiXmlElement* element );
+	virtual void WriteAttributes( FILE* fp );
+
+private:
+	static SettingsManager* instance;
+
+	int audioOn;
+	int nWalkingMaps;
+	grinliz::GLString path;
+};
+
+
+#endif // UFO_SETTINGS_INCLUDED
