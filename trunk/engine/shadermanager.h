21ee9166c220cd9e0dd48a59c41dc376227bcd92
leethomason@gmail.com <leethomason@gmail.com@d6cd19da-97b9-11dd-87e9-a30ea5446288>
2012-01-17 02:01:25 +0000

    updated sketch of the shader interface

--- trunk/engine/shadermanager.h
+++ trunk/engine/shadermanager.h
@@ -15,20 +15,23 @@ public:
 	~ShaderManager();
 
 	enum {
-		TEXTURE0			= 0x01,
-		TEXTURE0_TRANSFORM	= 0x02,
+		TEXTURE0			= 0x01,		// Texture0 is in use. Note that the sampling state (linear, nearest) is saved with the texture.
+		TEXTURE0_TRANSFORM	= 0x02,		// Texture0 has a texture transform.
 		TEXTURE1			= 0x04,
 		TEXTURE1_TRANSFORM	= 0x08,
-		NORMALS				= 0x10,
-		COLORS				= 0x20,
-		COLOR_MULTIPLIER	] 0x40,
-		LIGHTING_DIFFUSE	= 0x80
+		NORMALS				= 0x10,		// Per-vertex normals.
+		COLORS				= 0x20,		// Per-vertex colors.
+		COLOR_MULTIPLIER	= 0x40,		// Global color multiplier.
+		LIGHTING_DIFFUSE	= 0x80		// Diffuse lighting.
 	};
 
 	void ActivateShader( int flags );
 
+	// Texture units.
 	void SetTexture0( const Texture* );
 	void SetTexture1( const Texture* );
+
+	// Diffuse lighting parameters.
 	void SetDiffuse( const grinliz::Color4& ambient, const grinliz::Color4& diffuse );
 };
 #endif
